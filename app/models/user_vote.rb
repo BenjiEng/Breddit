130f12f498815ec85c52cb73fa2ce120e95787af
Ben E <benjamin.eng7@gmail.com>
2015-02-12 09:08:10 -0800

    push to heroku2

--- app/models/user_vote.rb
+++ app/models/user_vote.rb
@@ -1,11 +1,10 @@
 class UserVote < ActiveRecord::Base
   validates :user, presence: true
   # don't let the user vote twice
-  validates :user_id, uniqueness: { scope: [:votable_id, :votable_type] } #what is this?
-
+  validates :user_id, uniqueness: { scope: [:votable_id, :votable_type] }
   belongs_to :votable, polymorphic: true
 
-  belongs_to (
+  belongs_to(
     :user,
     class_name: "User",
     foreign_key: :user_id,
@@ -18,4 +17,5 @@ class UserVote < ActiveRecord::Base
     foreign_key: :post_id,
     primary_key: :id
   )
+
 end
