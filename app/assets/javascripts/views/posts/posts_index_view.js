eb438b497e9081af8d4dd5959b869ddcb16702cf
Ben E <benjamin.eng7@gmail.com>
2015-03-15 16:07:35 -0700

    Fix votes

--- app/assets/javascripts/views/posts/posts_index_view.js
+++ app/assets/javascripts/views/posts/posts_index_view.js
@@ -3,9 +3,7 @@ BredditApp.Views.PostsIndexView = Backbone.View.extend({
   tagName: "li",
   events: {
     'click .upvote': 'upvote',
-    'click .upvoted': 'deupvote',
     'click .downvote': 'downvote',
-    'click .downvoted': 'dedownvote',
   },
 
   initialize: function(){
@@ -13,66 +11,27 @@ BredditApp.Views.PostsIndexView = Backbone.View.extend({
   },
 
   upvote: function(event){
-    // var id = this.id
-    // var post = this.collection.get(id);
     var newUp = new BredditApp.Models.Upvote({post_id: this.model.id});
     var that = this;
     newUp.save({}, {
       success: function(){
-        // this.set('vote_count', post.get('vote_count') + 1);
-        this.set('vote_count', that.get('vote_count') + 1);
+        that.model.set('vote_count', that.model.get('vote_count') + 1);
         that.render();
         that.$(".upvote").removeClass('upvote').addClass('upvoted');
-        // that.$(".downvoted" + "[data-id='"+ id + "']").removeClass('downvoted').addClass('downvote');
+        that.$(".downvoted").removeClass('downvoted').addClass('downvote');
       }
     });
   },
 
   downvote: function(event){
-    var id = $(event.currentTarget).data('id');
-    var post = this.collection.get(id);
-    var newDown = new BredditApp.Models.Downvote({post_id: id});
-
+    var newDown = new BredditApp.Models.Downvote({post_id: this.model.id});
     var that = this;
     newDown.save({},{
       success: function(){
-        post.set('vote_count', post.get('vote_count') - 1);
+        that.model.set('vote_count', that.model.get('vote_count') - 1);
         that.render();
-        that.$(".downvote" + "[data-id='"+ id + "']").removeClass('downvote').addClass('downvoted');
-        that.$(".upvoted" + "[data-id='"+ id + "']").removeClass('upvoted').addClass('upvote');
-      }
-    });
-  },
-
-  deupvote: function(event){
-    var id = $(event.currentTarget).data('id');
-    var post = this.collection.get(id);
-    var newUp = new BredditApp.Models.Upvote({post_id: id});
-
-    var that = this;
-    newUp.save({}, {
-      success: function(){
-        post.set('vote_count', post.get('vote_count') - 1);
-        that.render();
-        that.$(".upvoted" + "[data-id='"+ id + "']").removeClass('upvoted').addClass('upvote');
-        that.$(".downvoted" + "[data-id='"+ id + "']").removeClass('downvoted').addClass('downvote');
-      }
-    });
-  },
-
-
-  dedownvote: function(event){
-    var id = $(event.currentTarget).data('id');
-    var post = this.collection.get(id);
-    var newUp = new BredditApp.Models.Upvote({post_id: id});
-
-    var that = this;
-    newUp.save({}, {
-      success: function(){
-        post.set('vote_count', post.get('vote_count') + 1);
-        that.render();
-        that.$(".downvoted" + "[data-id='"+ id + "']").removeClass('downvoted').addClass('downvote');
-        that.$(".upvoted" + "[data-id='"+ id + "']").removeClass('upvoted').addClass('upvote');
+        that.$(".downvote").removeClass('downvote').addClass('downvoted');
+        that.$(".upvoted").removeClass('upvoted').addClass('upvote');
       }
     });
   },
@@ -84,3 +43,53 @@ BredditApp.Views.PostsIndexView = Backbone.View.extend({
   },
 
 });
+
+
+// downvote: function(event){
+//   var id = $(event.currentTarget).data('id');
+//   var post = this.collection.get(id);
+//   var newDown = new BredditApp.Models.Downvote({post_id: id});
+//
+//   var that = this;
+//   newDown.save({},{
+//     success: function(){
+//       post.set('vote_count', post.get('vote_count') - 1);
+//       that.render();
+//       that.$(".downvote" + "[data-id='"+ id + "']").removeClass('downvote').addClass('downvoted');
+//       that.$(".upvoted" + "[data-id='"+ id + "']").removeClass('upvoted').addClass('upvote');
+//     }
+//   });
+// },
+
+// deupvote: function(event){
+//   var id = $(event.currentTarget).data('id');
+//   var post = this.collection.get(id);
+//   var newUp = new BredditApp.Models.Upvote({post_id: id});
+//
+//   var that = this;
+//   newUp.save({}, {
+//     success: function(){
+//       post.set('vote_count', post.get('vote_count') - 1);
+//       that.render();
+//       that.$(".upvoted" + "[data-id='"+ id + "']").removeClass('upvoted').addClass('upvote');
+//       that.$(".downvoted" + "[data-id='"+ id + "']").removeClass('downvoted').addClass('downvote');
+//     }
+//   });
+// },
+//
+//
+// dedownvote: function(event){
+//   var id = $(event.currentTarget).data('id');
+//   var post = this.collection.get(id);
+//   var newUp = new BredditApp.Models.Upvote({post_id: id});
+//
+//   var that = this;
+//   newUp.save({}, {
+//     success: function(){
+//       post.set('vote_count', post.get('vote_count') + 1);
+//       that.render();
+//       that.$(".downvoted" + "[data-id='"+ id + "']").removeClass('downvoted').addClass('downvote');
+//       that.$(".upvoted" + "[data-id='"+ id + "']").removeClass('upvoted').addClass('upvote');
+//     }
+//   });
+// },
